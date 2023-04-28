3150d2be39cde38dd65bfa4ee59b5fef50ed631c
TBSDrJ <marcus.jaiclin@bishops.com>
2023-04-28 16:12:42 -0700

    Create embedding.py

---
+++ embedding.py
@@ -0,0 +1,7 @@
+"""
+    Author: TBSDrJ
+    Date: Spring 2023
+    Purpose: Illustrate word embeddings along the lines of word2vec.
+    Reference: https://www.tensorflow.org/text/guide/word_embeddings
+"""
+
